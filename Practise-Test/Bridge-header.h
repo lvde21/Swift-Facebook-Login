618f1787b7b2c546185917962ab3781d6daa2431
Lala De <lvde21@live.com>
2015-06-08 17:28:29 +0800

    Login works with logout screen shown

---
+++ Practise-Test/Bridge-header.h
@@ -0,0 +1,18 @@
+//
+//  Bridge-header.h
+//  Practise-Test
+//
+//  Created by Lala Vaishno De on 6/8/15.
+//  Copyright (c) 2015 Casa Wee. All rights reserved.
+//
+
+#ifndef Practise_Test_Bridge_header_h
+#define Practise_Test_Bridge_header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+
+
+#endif
---
+++ Practise-Test/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  Practise-Test
+//
+//  Created by Lala Vaishno De on 6/8/15.
+//  Copyright (c) 2015 Casa Wee. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+        }
+        else
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.delegate = self
+        }
+        
+    }
+    
+    
+    // Facebook Delegate Methods
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        println("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+//            if result.grantedPermissions.contains("email")
+//            {
+//                // Do work
+//            }
+            self.returnUserData()
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        println("User Logged Out")
+    }
+    
+    
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                println("Error: \(error)")
+            }
+            else
+            {
+                println("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as NSString
+                println("User Name is: \(userName)")
+                let userEmail : NSString = result.valueForKey("email") as NSString
+                println("User Email is: \(userEmail)")
+                
+            }
+        })
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
